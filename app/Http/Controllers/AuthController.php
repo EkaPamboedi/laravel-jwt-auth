f8f7869b929ae2b956771fbfb26cf2ea0730413f
ekapamboedi <ekapamboedi@gmail.com>
2022-06-02 23:39:27 +0700

    succesfully registing

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+use Validator;
+
+
 
 class AuthController extends Controller
 {
-    //
+    public function _construct()
+    {
+      $this->middleware('auth:api', ['except' =>['login','register']]);
+    }
+
+    public function register(Request $request)
+    {
+      $validator = Validator::make($request->all(),
+      [
+        'name' => 'required',
+        'email' => 'required|string|email|unique:users',
+        'password'=>'required|string|confirmed|min:6'
+      ]);
+      if ($validator->fails()) {
+        return response()->json($validator->errors()->toJson(),400);
+      }
+      $user = User::create(array_merge(
+        $validator->validated(),
+        ['password'=>bcrypt($request->password)]
+      ));
+
+      return response()->json([
+        'massage'=> 'User succesfully registered',
+        'user'=>$user
+      ],201);
+    }
+
+    public function login(Request $request)
+    {
+      
+    }
 }
